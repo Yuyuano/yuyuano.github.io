7586e7a42f102537dde477650e0eb368dc21afdd
Yuyuan <2058920169@qq.com>
2025-11-13 21:33:54 +0800

    修正帖子内容与更改代码块背景颜色与随机播放歌曲

--- src/config.ts
+++ src/config.ts
@@ -319,7 +319,7 @@ export const licenseConfig: LicenseConfig = {
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// 注意：某些样式（如背景颜色）已被覆盖，请参阅 astro.config.mjs 文件。
 	// 请选择深色主题，因为此博客主题目前仅支持深色背景
-	theme: "github-dark",
+	theme: "one-dark",
 };
 
 export const commentConfig: CommentConfig = {
